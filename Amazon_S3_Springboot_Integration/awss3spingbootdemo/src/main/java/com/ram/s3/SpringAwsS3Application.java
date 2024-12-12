f71abe6da898f46cd04b69b97c0073e05db4dacc
Ram N <ramram_43210@yahoo.com>
2024-12-12 18:43:40 +0530

    1

---
+++ Amazon_S3_Springboot_Integration/awss3spingbootdemo/src/main/java/com/ram/s3/SpringAwsS3Application.java
@@ -0,0 +1,16 @@
+package com.ram.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringAwsS3Application
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringAwsS3Application.class, args);
+	}
+	
+}
